64f803053fd2caf0f9abde048de10994902c71fc
root <Easlee Liu>
2018-07-25 15:31:10 +0000

    add borker test

--- api_test.go
+++ api_test.go
@@ -3,9 +3,19 @@ package net
 import (
 	"testing"
 
+	"github.com/xtech-cloud/omo-mod-net/processor"
+	"github.com/xtech-cloud/omo-mod-net/protocol"
 	"github.com/xtech-cloud/omo-mod-net/service"
 )
 
+func handlePing(_req *protocol.Request, _rsp *protocol.Response) {
+	_rsp.Head.Msg = "pong"
+	_rsp.Body = &protocol.EmptyBlock{}
+}
+
 func Test_RunBroker(_t *testing.T) {
-	service.RunBroker("test", "tcp://*.9999", "tcp://*.9990")
+	go service.RunBroker("test", "tcp://*:3000", "tcp://*:9990")
+
+	processor.BindJsonHandler("ping", handlePing)
+	service.RunWorker("test", "tcp://127.0.0.1:9990", processor.ProcessJson)
 }
